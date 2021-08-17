a13a0a2148f6fa625f527ae7d4cfa56a894c485b
Simeon Bartley <simeon.bartley@unicarehealth.com.au>
2021-08-17 09:42:08 +0800

    Add useful default styles/types (success/warn/info/error)

---
+++ lib/showToast.d.ts
@@ -0,0 +1,5 @@
+import { CSSResultGroup } from "lit";
+import { LitToast } from "./lit-toast";
+export declare type ExtraClassType = "error" | "success" | "info" | "warning";
+export declare function showToast(toastEl: LitToast, msg: string, className?: ExtraClassType, duration?: number): Promise<void>;
+export declare function toastStyles(): CSSResultGroup;
